b133f9a712fef072aff69386081ced2dfd5e0342
Andre Grech <silver.grech@gmail.com>
2020-11-16 14:32:33 +1100

    burgers model created and exported. calling on orm functions, matching inputs if required.

---
+++ models/burgers.js
@@ -0,0 +1,28 @@
+const orm = require('../config/orm');
+
+const burger = {
+  selectAll: (callback) => {
+    orm.selectAll("burgers", (res) => {
+      callback(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  insertOne: (columns, values, callback) => {
+    orm.insertOne("burgers", columns, values, (res) => {
+      callback(res);
+    });
+  },
+  updateOne: (objColVals, condition, callback) => {
+    orm.updateOne("burgers", objColVals, condition, (res) => {
+      callback(res);
+    });
+  },
+  delete: (condition, callback) => {
+    orm.delete("burgers", condition, (res) => {
+      callback(res);
+    });
+  }
+};
+
+// Export the database functions for the controller (catsController.js).
+module.exports = burger;
