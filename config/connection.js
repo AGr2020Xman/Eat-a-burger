fc00713b9052a17e1ce33162be0cd7e839591373
Andre Grech <silver.grech@gmail.com>
2020-11-16 23:15:20 +1100

    jawsDB conenction info added

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,27 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'H0n@s0up1234',
-    database: 'burger_db',
-});
+//JAWSDB connection
+let connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'H0n@s0up1234',
+        database: 'burger_db',
+    });
+};
+
+// Local development connection
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     port: 3306,
+//     user: 'root',
+//     password: '',
+//     database: 'burger_db',
+// });
 
 connection.connect((err)=>{
     if (err) {
